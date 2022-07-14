20151950980f7ef5e68fdb227c1d64a99167eb2f
Nelson Tapia <ntapiapinto@gmail.com>
2022-07-13 20:53:29 -0400

    first commit

---
+++ Contact App CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact App CoreData
+//
+//  Created by nelson tapia on 13-07-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
